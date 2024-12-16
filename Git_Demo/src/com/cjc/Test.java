f75b309e3ed14eabea1f4e4ccd7e1b80effa8b30
Girish <Girish@LAPTOP-7IKR08SV>
2024-12-16 15:30:22 +0530

    add m1 method

--- Git_Demo/src/com/cjc/Test.java
+++ Git_Demo/src/com/cjc/Test.java
@@ -2,6 +2,10 @@ package com.cjc;
 
 public class Test {
 
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("This is Github Project");
