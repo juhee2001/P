cf4cfbd4487e2284f5c7574471ab6ae8eabdd5ba
juhee2001 <juheeupadhye13@gmail.com>
2024-12-16 15:31:37 +0530

    add m2 method

--- Git_Demo/src/com/cjc/Test.java
+++ Git_Demo/src/com/cjc/Test.java
@@ -6,6 +6,10 @@ public class Test {
 	{
 		System.out.println("m1 method");
 	}
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
 	public static void main(String args[])
 	{
 		System.out.println("This is Github Project");
